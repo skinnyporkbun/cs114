faf420cfb4538f0319f0ee8895992517b366ebfb
s203 <s203@ai241lab0203m11.aipdcls.edu>
2017-07-20 16:15:02 -0700

    added more timers for more dynamic conversation

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,5 @@
+import time
+
 print ("Hello world")
 time.sleep( 2 )
 print ("How are you feeling today from 1 to 10? 1 being bad and 10 being good.")
@@ -11,8 +13,7 @@ if 0 < feeling <= 5:
 else:
     if 6 <= feeling <= 10:
         print ("Well you seem be having a wonderful day! Hope it stays that way!")
-
+time.sleep( 3 )
 print ("Was a pleasure to hear about your day. Unfortunately, I'll be shutting down now so I'll be seeing you around!")
-import time
 time.sleep( 5 )
 exit()
