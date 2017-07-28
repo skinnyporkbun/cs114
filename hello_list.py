1f35ea5c51420d716ae9f83a9e9dfa5486fc1957
s203 <s203@ai241lab0203m11.aipdcls.edu>
2017-07-27 17:16:15 -0700

    Finally got list and skip working

--- hello_list.py
+++ hello_list.py
@@ -1,29 +1,28 @@
 from time import sleep
 
 print ("Hello world")
-time.sleep( 2 )
+sleep( 2 )
 print ("How are you feeling today from 1 to 10? 1 being bad and 10 being good.")
-time.sleep( 3 )
 feeling = input("Tell me here: ")
 
-if feeling = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-    int(feeling)
-else:
-    if feeling = "skip"
-        print("Oh, okay. I won't ask you again then")
+if feeling == "1" or "2" or "3" or "4" or "5" or "6" or "7" or "8" or "9" or "10":
+
+    if feeling == "skip":
+           print("Oh, okay. I won't ask you again then")
+    elif 0 < int(feeling) <= 5:
+        print ("Aww, well that sucks. Hope you feel better")
+
+    elif 6 <= int(feeling) <= 10:
+            print ("Well you seem be having a wonderful day! Hope it stays that way!")
+
+# if feeling == "skip":
+#         print("Oh, okay. I won't ask you again then")
 
 
-if 0 < feeling <= 5:
-    print ("Aww, well that sucks. Hope you feel better")
 
-else:
-    if 6 <= feeling <= 10:
-        print ("Well you seem be having a wonderful day! Hope it stays that way!")
-#    else:
-#        if feeling = str("skip"):
-#            print("Oh, okay. I won't ask you again then")
-time.sleep( 3 )
+
+sleep( 3 )
 print ("Unfortunately, I'll be shutting down now so I'll be seeing you around!")
-time.sleep( 5 )
+sleep( 5 )
 
 print("HELLO FUTURE ME :D - Voicemail left on Thursday, July 20th, 2017")
