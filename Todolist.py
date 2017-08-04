0ecba9956b99817b2edb4be976e561c96486a7e7
s203 <s203@ai241lab0203m11.aipdcls.edu>
2017-08-03 17:50:40 -0700

    attempted to fix

--- Todolist.py
+++ Todolist.py
@@ -1,25 +1,25 @@
 reminder = []
 
-print ("Hello! What would you like to remember today?")
-print ("Please type out the first thing here: ")
-reminder.append(input(str()))
-
-print ("Would you like to be reminded of anything else?")
-yesno = str(input())
+# print ('Hello! What would you like to remember today?')
+# print ('Please type out the first thing here: ')
+# firstitem = input()
+# reminder.append(firstitem)
+print ('Would you like to be reminded of anything else?')
+yesno = input()
 
 # Create a loop to continue asking for more whilst user says YES and stop when user says NO
 # loopcheck = 0
 # while loopcheck == 0:
 #
-if str(yesno) == "Yes" or "YES" or "yes" or "y" or "Y":
-    print ("What else is it that you would like to remember?")
+if yesno == ('Yes' or 'YES' or 'yes' or 'y' or 'Y'):
+    print ('What else is it that you would like to remember?')
     reminder.append(input(str()))
-    print ("Would you like to be reminded of anything else? Yes or no?")
-elif str(yesno) == "No" or "NO" or "no" or "n" or "N":
-    print ("Alright then, here's what you still need to do")
-# else:
-#     print ("it broke")
+    print ('Would you like to be reminded of anything else? Yes or no?')
+elif yesno == ('No' or 'NO' or 'no' or 'n' or 'N'):
+    print ('Alright then, here\'s what you still need to do')
+else:
+    print ('it broke')
 
-# if yesno == "No" or "NO" or "no" or "n" or "N":
-#     print ("OKAY BYE")
+# if yesno == 'No' or 'NO' or 'no' or 'n' or 'N':
+#     print ('OKAY BYE')
 print (reminder)
